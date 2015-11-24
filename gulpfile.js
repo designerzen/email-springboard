d8bdd7b792caeea027b43ed04c0f65665dbfd851
designerzen <designerzen@gmaill.com>
2015-11-24 17:07:07 +0000

    Added in updated dependencies and removed Premailer as was Ruby Loving and w~indows Hatin

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,32 @@
+// =======================---------------- IMPORT DEPENDENCIES --------------------
+
+var Styliner = require('styliner');
+
+// Requirements for this build to work :
+var gulp = require('gulp');						// main Gulp
+var concat = require('gulp-concat');			// combine files
+
+// JS Plugins
+var minify = require('gulp-minify');			// squash files
+
+var newer = require('gulp-newer');				// deal with only modified files
+var imagemin = require('gulp-imagemin');		// squish images
+
+// CSS Plugins
+var less = require('gulp-less');				// compile less files to css
+
+// HTML Plugins
+var jade = require('gulp-jade');
+var premailer = require('gulp-premailer');
+var htmlmin = require('gulp-htmlmin');
+
+var del = require('del');						// delete things and folders
+var sequencer = require('run-sequence');
+var path = require('path');
+var fs = require('fs');
+
+var livereload = require('gulp-livereload');
+
 /*
 
 This is just a handy Gulp Boilerplate that
@@ -73,36 +102,54 @@ Prints additional information at runtime.
 
 */
 
-// =======================---------------- IMPORT DEPENDENCIES --------------------
-
-// Requirements for this build to work :
-var gulp = require('gulp');						// main Gulp
-var concat = require('gulp-concat');			// combine files
-
-// JS Plugins
-var minify = require('gulp-minify');			// squash files
-
-var newer = require('gulp-newer');				// deal with only modified files
-var imagemin = require('gulp-imagemin');		// squish images
-
-// CSS Plugins
-var less = require('gulp-less');				// compile less files to css
-
-// HTML Plugins
-var jade = require('gulp-jade');
-var premailer = require('gulp-premailer');
-var htmlmin = require('gulp-htmlmin');
-
-var del = require('del');						// delete things and folders
-var sequencer = require('run-sequence');
-
-var livereload = require('gulp-livereload');
-// =======================---------------- CONFIGURATION --------------------
 
 // Set up paths here!
-var SOURCE_FOLDER 			= 'src/';		// Source files Root
-var BUILD_FOLDER 			= 'build/';		// Where the initial build occurs (debugable)
+var SOURCE_FOLDER 				= 'src/';		// Source files Root
+var BUILD_FOLDER 					= 'build/';		// Where the initial build occurs (debugable)
 var DISTRIBUTION_FOLDER 	= 'dist/';		// Once debugging is complete, copy to server ready files here
+
+
+//Options
+var options = {};
+
+// True to minify all output. This option removes all extraneous whitespace from the generated HTML (including any remaining inline stylesheets)
+options.compact = true;
+
+// True to not emit <style> tags for rules that cannot be inlined.
+// This option will completely drop any dynamic CSS rules.
+// (such as media queries, pseudo-elements, or @font-faces)
+options.noCSS = true;
+
+// True to keep all rules in <style> tags instead of inlining static rules into elements.
+// This results in smaller files, but will not work with Gmail.
+options.keepRules = false;
+
+// True to add an attribute/ID selector to all rules in media queries to fix a bug in Yahoo Mail.
+// Yahoo Mail drops all media queries, converting their contents to regular rules that will always be applied.
+// This option adds a workaround for that.
+options.fixYahooMQ = true;
+
+// Don't skip properties that parserlib reports as invalid. (all invalid properties are always logged as winston errors)
+// Pass this option if you're using features that parser doesn't recognize, or if you come across a bug in parserlib
+// This option breaks Acid2, which tests that valid properties from earlier rules replace invalid properties from later rules. (see also the first known issue)
+options.keepInvalid = true;
+
+// The path containing referenced URLs.
+// All non-absolute URLs in <a> tags, <img> tags, and stylesheets will have this path prepended.
+// For greater flexibility, pass a url() function instead.
+// options.urlPrefix = "dir/";
+
+// A function called to resolve URLs.
+// All non-absolute URLs in HTML or CSS will be replaced by the return value of this function.
+// The function is passed the relative path to the file and the source of the URL ("img" or "a" or other HTML tags; URLs from CSS pass "img").
+// It can return a promise or a string.
+// options.url = function(path, type);
+
+
+
+
+
+// =======================---------------- CONFIGURATION --------------------
 
 // Where do our source files live?
 var source = {
@@ -116,11 +163,16 @@ var source = {
 };
 
 // Where shall we compile them to?
-var destination = {
-	styles 	: BUILD_FOLDER,
-	html 	: BUILD_FOLDER,
-	images	: BUILD_FOLDER
+var generateDestinations = function( rootPath ) {
+	return {
+		styles 	: rootPath,
+		html 	: rootPath,
+		images	: rootPath
+	}
 };
+
+var distribution = generateDestinations( BUILD_FOLDER );
+var destination = generateDestinations( DISTRIBUTION_FOLDER );
 
 var imageCrunchOptions = {
 	optimizationLevel: 3,
@@ -138,6 +190,25 @@ var htmlSquishOptions = {
 	keepClosingSlash   : true
 };
 
+////////////////////////////////////////////////////////////////////////////////
+// Ensure that a directory exists and if it doesn't, create it!
+////////////////////////////////////////////////////////////////////////////////
+var ensureExists = function(path, mask, cb)
+{
+    if (typeof mask == 'function') { // allow the `mask` parameter to be optional
+        cb = mask;
+        mask = 0777;
+    }
+    fs.mkdir(path, mask, function(err) {
+        if (err) {
+            if (err.code == 'EEXIST') cb(null); // ignore the error if the folder already exists
+            else cb(err); // something else went wrong
+        } else cb(null); // successfully created folder
+    });
+}
+
+
+
 // =======================---------------- TASK DEFINITIONS --------------------
 
 gulp.task('clean', function(cb) {
@@ -152,7 +223,8 @@ gulp.task('jade', function() {
 	// with sourcemaps all the way down
 	return 	gulp.src( source.jade )
 			.pipe( jade( { pretty:false, debug:false, compileDebug:false } ) )
-			.pipe( gulp.dest( destination.html ) );
+			.pipe( gulp.dest( destination.html ) )
+			.pipe( gulp.dest( distribution.html ) );
 });
 
 gulp.task('compile-examples', function() {
@@ -160,15 +232,55 @@ gulp.task('compile-examples', function() {
 	// with sourcemaps all the way down
 	return 	gulp.src( source.jade_examples )
 			.pipe( jade( { pretty:false, debug:false, compileDebug:false } ) )
-			.pipe( gulp.dest( destination.html ) );
+			.pipe( gulp.dest( destination.html ) )
+			.pipe( gulp.dest( distribution.html ) );
 });
 
 gulp.task('assemble', function () {
-    return 	gulp.src( destination.html +'*.html')
+    return gulp.src( destination.html +'*.html')
 			.pipe( premailer(premailerOptions) )
 			.pipe( htmlmin(htmlSquishOptions) )
-			.pipe( gulp.dest(DISTRIBUTION_FOLDER) );
+			.pipe( gulp.dest( destination.html ) )
+			.pipe( gulp.dest( distribution.html ) );
 });
+
+
+// Convert an HTML page into a ENCAPSULATED HTML page
+gulp.task('convert', function( cb ){
+
+	var inputDir = path.join( __dirname , destination.html );
+	var inputFile = path.join( inputDir, 'email.html' );
+
+	var destinationDir = path.join( __dirname , DISTRIBUTION_FOLDER );
+	var destinationFile = path.join( destinationDir, 'email.html' );// load in email
+
+	//if (err) console.log('Creating dir '+destinationDir);// handle folder creation error
+  //else console.log('dir creating '+destinationDir);// we're all good
+
+	var styleInliner = new Styliner( inputDir, options );
+	var originalSource = fs.readFileSync( inputFile, 'utf8');
+
+	ensureExists( destinationDir, 0744, function(err) {
+
+		styleInliner.processHTML( originalSource ).then( function(processedSource) {
+
+			// save locally as utf8!
+			fs.writeFile( destinationFile, processedSource, 'utf8', function (err) {
+				//console.log("Writing to file "+destinationFile);
+				//console.log(processedSource);
+					console.log('Saved ', destinationFile);
+					if (err) throw err;
+					return cb(err);
+			});
+
+		});
+
+	});
+
+});
+
+
+
 
 // Image Tasks ===================================================
 
@@ -178,7 +290,8 @@ gulp.task('images', function() {
 			.pipe( newer(destination.images) )
 			// Pass in options to the task
 			.pipe( imagemin( imageCrunchOptions ) )
-			.pipe( gulp.dest( destination.images ) );
+			.pipe( gulp.dest( destination.images ) )
+			.pipe( gulp.dest( distribution.images ) );
 });
 
 
@@ -193,7 +306,8 @@ gulp.task('css', function() {
 			// lint
 			// squish
 			//.pipe( minify() )
-			.pipe( gulp.dest( destination.styles ) );
+			.pipe( gulp.dest( destination.styles ) )
+			.pipe( gulp.dest( distribution.styles ) );
 });
 
 
@@ -206,7 +320,7 @@ gulp.task('watch', function() {
   livereload.listen();
 
   // Watch any files in dist/, reload on change
-  gulp.watch(['dist/**']).on('change', livereload.changed);
+  gulp.watch(['src/**']).on('change', livereload.changed);
 
 });
 // =======================---------------- TASKS --------------------
@@ -232,7 +346,7 @@ gulp.task('watch', function() {
 gulp.task('build', 		[ 'css', 'jade', 'images' ] );
 
 // squish everything & concatanate scripts
-gulp.task('deploy', 	[ 'build', 'assemble' ] );
+gulp.task('deploy', 	[ 'build', 'convert' ] );
 gulp.task('distribute', ['deploy'] );
 
 // create a server to host this project
@@ -243,7 +357,7 @@ gulp.task('examples', function(callback) {
 	sequencer(
 		'clean',
 		[ 'css', 'compile-examples', 'images' ],
-		'assemble',
+		'convert',
     callback);
 });
 
@@ -253,6 +367,6 @@ gulp.task('default', function(callback) {
 	sequencer(
 		'clean',
 		'build',
-		'assemble',
+		'convert',
     callback);
 });
